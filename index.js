47c288dec7dd619fb31b4fd4fbbbf0e82309a5e2
Daniel Cheong <dcheo001@gmail.com>
2021-11-04 22:28:06 -0400

    commit tree 1121f0ba4c2e40fff97489802bb774384eec9a71

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+// Code your solution here
+
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+let totalBatteries = batteryBatches.reduce(function(totalBatteries, currentValue) {
+    return totalBatteries + currentValue
+})
+console.log(totalBatteries);
+
+
